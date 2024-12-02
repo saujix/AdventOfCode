49415cc44328387dc6c28da9ad58e63c37a24e32
saujix <chutkipasbola@gmail.com>
2024-12-02 20:40:20 +0000

    ah man tf was that

--- 2024/2.12.24/q1.py
+++ 2024/2.12.24/q1.py
@@ -17,46 +17,48 @@ findif=[]
 pos=0
 neg=0
 
-while(i < len(totalArray)):
-#while(i < 1):
-    
-    #nth element of the array, this is the element we are working with
-    currentArray = totalArray[i]
+#gets the difference of consecutive elements and appends them to the dif array
+def diffArray(array):
     j = 0
-    
-
-    #gets the difference of consecutive elements and appends them to the dif array
-    while(j<len(currentArray)-1):
-        dif.append(int(currentArray[j+1])-int(currentArray[j]))
+    while(j<len(array)-1):
+        dif.append(int(array[j+1])-int(array[j]))
         j+=1
-    
+    return dif
 
-    #this is to check the diff nature between different elements
-    for t in dif:
+def checkArray(array):
+    pos=0
+    neg=0
+    for t in array:
         if t>0:
             pos+=1
         elif t<0:
             neg+=1
-    
-    status_unsafe=0
+    return pos, neg
 
-    #
+def checkunSafe(pos, neg):
+    status_unsafe=0
     if (pos > 0) != (neg > 0): 
         for t in dif:
             if abs(t) not in range(1, 4):
                 status_unsafe +=1
-    
-    if status_unsafe>0:
-        unsafe+=1
-    else:
-        safe+=1
+    return status_unsafe
 
-    #resetting
-    pos=0
-    neg=0
-    dif=[]
-    status_unsafe=0
+
+
+
+while(i < len(totalArray)):
+
+    currentArray = totalArray[i]
     
+    dif = diffArray(currentArray)
+
+    pos=checkArray(dif)[0]
+    neg=checkArray(dif)[1]   
+
+    if checkunSafe(pos,neg)==0:
+        safe+=1
+        
+  
     i+=1
 print(safe)
 print(unsafe)
