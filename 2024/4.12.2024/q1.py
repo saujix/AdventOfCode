05926e9cbcef6f5b3cc49f449470a7a8bb58cc1d
saujix <chutkipasbola@gmail.com>
2024-12-04 20:45:22 +0000

    hell

--- 2024/4.12.2024/q1.py
+++ 2024/4.12.2024/q1.py
@@ -7,87 +7,25 @@ with open("input.txt", 'r') as file:
         dataArray.append(line.strip())
 
 count = 0
-
-def lookUp(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x][y-1] =="M" and data[x][y-2]=="A" and data[x][y-3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def lookDown(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x][y+1] =="M" and data[x][y+2]=="A" and data[x][y+3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def lookRight(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x+1][y] =="M" and data[x+2][y]=="A" and data[x+3][y]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def lookLeft(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x-1][y] =="M" and data[x-2][y]=="A" and data[x-3][y]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def looktopLeft(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x-1][y-1] =="M" and data[x-2][y-2]=="A" and data[x-3][y-3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def looktopRight(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x+1][y-1] =="M" and data[x+2][y-2]=="A" and data[x+3][y-3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def lookbottomLeft(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x-1][y+1] =="M" and data[x-2][y+2]=="A" and data[x-3][y+3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
-def lookbottomRight(data, x, y):
-    try:
-        if data[x][y] =="X" and data[x+1][y+1] =="M" and data[x+2][y+2]=="A" and data[x+3][y+3]=="S":
-            return 1
-    except:
-        pass
-    return 0
-
+operations = [[1,0],
+              [0,1],
+              [1,1],
+              [-1,0],
+              [0,-1],
+              [-1,-1],
+              [-1,1],
+              [1,-1]
+              ]
 x = 0
 y = 0
 
-for y in range(140):
-    for x in range(140):
-        count += lookUp(dataArray,x,y)
-        count+= lookDown(dataArray,x,y)
-        count+= lookRight(dataArray,x,y)
-        count+= lookLeft(dataArray,x,y)
-        count+=looktopLeft(dataArray, x, y)
-        count+=looktopRight(dataArray, x, y)
-        count+=lookbottomLeft(dataArray, x, y)
-        count+=lookbottomRight(dataArray, x, y)
+for x in range(140):
+    for y in range(140):
+        for i in operations:
+            try:
+                if dataArray[x][y] =="X" and dataArray[x+i[0]][y+i[1]] =="M" and dataArray[x+2*(i[0])][y+2*(i[1])]=="A" and dataArray[x+3*(i[0])][y+3*(i[1])]=="S":
+                    count+=1
+            except:
+                pass
+        
 print(count)
-
-
-    
-
-
