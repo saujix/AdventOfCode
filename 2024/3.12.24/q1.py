4438320e2854b37ab608b66e35b00247a95493d9
saujix <chutkipasbola@gmail.com>
2024-12-04 00:07:58 +0000

    womp womp

--- 2024/3.12.24/q1.py
+++ 2024/3.12.24/q1.py
@@ -4,7 +4,6 @@ multiply = 0
 with open('input.txt', 'r') as file:
     content = file.read()
     match = re.findall(r"mul\((\d+),(\d+)\)", content)
-    print(match)
     for x in match:
         multiply += int(x[0]) * int(x[1])
 
